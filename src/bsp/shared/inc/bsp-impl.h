50b124f48144d0c54873fcfa3b3edcad380dd105
samuel-krain <samuel.k@lunasaspace.com>
2024-08-20 12:08:12 +0100

    Add notice of modifications to files as per Apache Licence 2.0

--- src/bsp/shared/inc/bsp-impl.h
+++ src/bsp/shared/inc/bsp-impl.h
@@ -4,7 +4,8 @@
  * Copyright (c) 2020 United States Government as represented by the
  * Administrator of the National Aeronautics and Space Administration.
  * All Rights Reserved.
- *
+ * Modifications copyright (c) 2024 Lunasa Ltd.
+ * 
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License. You may obtain
  * a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
@@ -14,6 +15,9 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
+ * 
+ * This file has been modified from the original version provided by the
+ * National Aeronautics and Space Administration under the Apache License 2.0
  ************************************************************************/
 
 /**
